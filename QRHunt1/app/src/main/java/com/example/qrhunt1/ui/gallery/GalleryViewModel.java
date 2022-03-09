54a9233700f585e7a1417714928de5c9c9c39e04
“Ming <mye2@ualberta.ca>
2022-03-08 22:19:25 -0700

    created bottom navigation bar

---
+++ QRHunt1/app/src/main/java/com/example/qrhunt1/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,15 @@
+package com.example.qrhunt1.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    //private final MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+
+    }
+
+}
