93867d651f25efb544ffa68090029eaa005873d2
“Ming <mye2@ualberta.ca>
2022-03-13 22:57:36 -0600

    Completed some functions of Gallery Section and My Profile Section

---
+++ QRHunter_v1/app/src/main/java/com/example/qrhunt1/GameQRCode.java
@@ -0,0 +1,73 @@
+package com.example.qrhunt1;
+
+
+import android.graphics.Bitmap;
+
+import java.util.ArrayList;
+
+public class GameQRCode {
+    private int score;
+    private String hashcode;
+    private String comment = "";
+    private String location;
+    private Bitmap QRImage;
+
+
+    /**
+     * Initialize Game QR code class。
+     */
+    GameQRCode(String hashcode){
+        //comment = "A QR code on a store poster";    //testing
+        score = calculateScore();
+    }
+
+    /**
+     * Needs to be done: Calculate the score of the game QR code.
+     */
+    private int calculateScore(){
+
+        return 0;
+    }
+    /**
+     * Return the score of the game QR code.
+     */
+    public int getScore() {
+        return score;
+    }
+    /**
+     * Add a comment to a game QR code.
+     */
+    public void addComment(String theComment) {
+        comment = theComment;
+    }
+    /**
+     * Delete a comment to a game QR code.
+     */
+    public void deleteComment(String theComment) {
+        comment = "";
+    }
+    /**
+     * return the comment of a game QR code.
+     */
+    public String getComments() {
+        return comment;
+    }
+
+    public void setLocation(String newlocation) { location = newlocation; }
+
+    /**
+     * return the location of a game QR code.
+     */
+    public String getLocation() {
+        return location;
+    }
+
+
+    public void setQRImage(Bitmap newImage) {
+        QRImage = newImage;
+    }
+    public Bitmap getQRImage() {
+        return QRImage;
+    }
+
+}
---
+++ QRHunter_v1/app/src/main/java/com/example/qrhunt1/GameQRList.java
@@ -0,0 +1,57 @@
+package com.example.qrhunt1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class GameQRList extends ArrayAdapter<GameQRCode> {
+
+    private ArrayList<GameQRCode> codes;
+    private Context context;
+
+
+    public GameQRList( Context context, int resource, ArrayList<GameQRCode> codes) {
+        super(context,0, codes);
+        this.codes = codes;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View view = convertView;
+
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.item_gallerylist, parent, false);
+        }
+        GameQRCode code = null;
+        if (codes.size() != 0){
+            code = codes.get(position);
+
+        }
+        if (code == null) {
+            return view;
+        }
+
+        TextView gameQRCodeScore = view.findViewById(R.id.qr_score);
+        TextView gameQRCodeLocation = view.findViewById(R.id.qr_location);
+        TextView gameQRCodeComment = view.findViewById(R.id.qr_comment);
+
+        gameQRCodeScore.setText("Score: " + code.getScore());
+        gameQRCodeLocation.setText("Location: " + code.getLocation());
+        gameQRCodeComment.setText(code.getComments());
+
+        return view;
+    }
+
+
+}
