23051ef42180da6d18b42276f0097a66b1a37ab9
feiyang412 <feiyang2@ualberta.ca>
2022-03-14 16:01:07 -0600

    update search username and player's ranking part

--- QRHunter_v1/app/src/main/java/com/example/qrhunt1/ui/players/PlayersFragment.java
+++ QRHunter_v1/app/src/main/java/com/example/qrhunt1/ui/players/PlayersFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,8 +27,57 @@ public class PlayersFragment extends Fragment {
         binding = FragmentPlayersBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textPlayers;
-        playersViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //final EditText editText = binding.editText;
+        //playersViewModel.getText().observe(getViewLifecycleOwner(), editText::setText);
+
+        Button searchButton = binding.searchButton;
+        EditText searchUser = binding.editText;
+        Button bestQRButton = binding.bestQR;
+        Button totalQRsButton = binding.totalQRs;
+        Button totalScoreButton = binding.totalScore;
+        ListView bestQRList = binding.bestQRList;
+        ListView totalQRsList = binding.totalQRsList;
+        ListView totalScoreList = binding.totalScoreList;
+
+        String username = searchUser.getText().toString();
+        //click this button
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //to searched user's profile
+            }
+        });
+
+        //click this button and show the ranking for best QR
+        bestQRButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bestQRList.setVisibility(View.VISIBLE);
+                totalQRsList.setVisibility(View.GONE);
+                totalScoreList.setVisibility(View.GONE);
+            }
+        });
+
+        //click this button and show the ranking for Total QRs
+        totalQRsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bestQRList.setVisibility(View.GONE);
+                totalQRsList.setVisibility(View.VISIBLE);
+                totalScoreList.setVisibility(View.GONE);
+            }
+        });
+
+        //click this button and show the ranking for Total QRs
+        totalScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bestQRList.setVisibility(View.GONE);
+                totalQRsList.setVisibility(View.GONE);
+                totalScoreList.setVisibility(View.VISIBLE);
+            }
+        });
+
         return root;
     }
 
@@ -35,3 +87,4 @@ public class PlayersFragment extends Fragment {
         binding = null;
     }
 }
+
