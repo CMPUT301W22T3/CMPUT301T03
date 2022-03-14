2e840cc1882d964b8d30a2d32210f6f42fdde740
zhixuan Liu <zhixuan1@ualberta.ca>
2022-03-14 01:18:59 -0600

    update the latest version

--- QRHunter_v1/app/src/main/java/com/example/qrhunt1/ui/profile/MyProfileFragment.java
+++ QRHunter_v1/app/src/main/java/com/example/qrhunt1/ui/profile/MyProfileFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,23 +21,119 @@ import java.util.Collections;
 
 public class MyProfileFragment extends Fragment {
 
-    private FragmentMyProfileBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        MyProfileViewModel MyProfileViewModel =
-                new ViewModelProvider(this).get(MyProfileViewModel.class);
+        View view = inflater.inflate(R.layout.fragment_my_profile,container,false);
 
-        binding = FragmentMyProfileBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-//        final TextView textView = binding.textProfile;
-//        MyProfileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        //  My profile
+        TextView text1 = view.findViewById(R.id.textView);
+        TextView text2 = view.findViewById(R.id.textView2);
+        TextView text10 = view.findViewById(R.id.textView10);
+        TextView text11 = view.findViewById(R.id.textView11);
+        TextView text12 = view.findViewById(R.id.textView12);
+        TextView text13 = view.findViewById(R.id.textView13);
+        TextView text14 = view.findViewById(R.id.textView14);
+        TextView text15 = view.findViewById(R.id.textView15);
+        TextView text16 = view.findViewById(R.id.textView16);
+        Button button = view.findViewById(R.id.button);
+
+
+//  Input username
+        String user = "Username: John";
+        text1.setText(user);
+
+//  Input Contact info
+        String phoneNumber = "Address: Tree Street";
+        text2.setText(phoneNumber);
+
+//  Highest QR code
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(10);
+        list.add(3);
+        list.add(11);
+        list.add(2);
+        Collections.sort(list);
+        Integer a = list.get(list.size()-1);
+        String d = Integer.toString(a);
+        text10.setText(d);
+
+//  lowest QR code
+        Collections.reverse(list);
+        String b = list.get(list.size()-1).toString();
+        text11.setText(b);
+
+//  Sum of Scores #
+        int i;
+        int sum=0;
+        for (i = 0; i < list.size(); i++)
+            sum += list.get(i);
+        String c = Integer.toString(sum);
+        text12.setText(c);
+
+//  Total number of QR codes
+        Integer num = list.size();
+        String string = Integer.toString(num);
+        text13.setText(string);
+
+//  Ranking in Best QR
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(11);
+        list1.add(5);
+        list1.add(18);
+        list1.add(5);
+        list1.add(15);
+        Collections.sort(list1);
+        Collections.reverse(list1);
+        Integer index = list1.indexOf(11)+1;
+        String e = Integer.toString(index);
+        text14.setText(e);
+
+//  Ranking in Total QRs
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(5);
+        list2.add(2);
+        list2.add(2);
+        list2.add(15);
+        list2.add(0);
+        Collections.sort(list2);
+        Collections.reverse(list2);
+        Integer index1 = list2.indexOf(5)+1;
+        String f = Integer.toString(index1);
+        text15.setText(f);
+
+//  Ranking in Total Score
+        ArrayList<Integer> list3 = new ArrayList<>();
+        list3.add(27);
+        list3.add(78);
+        list3.add(25);
+        list3.add(67);
+        list3.add(60);
+        Collections.sort(list3);
+        Collections.reverse(list3);
+        Integer index2 = list3.indexOf(27)+1;
+        String g = Integer.toString(index2);
+        text16.setText(g);
+
+//  Login QR button
+        ImageView QR1 = view.findViewById(R.id.imageView);
+        ImageView QR2 = view.findViewById(R.id.imageView3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                QR1.setVisibility(View.INVISIBLE);
+                QR2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        return view;
     }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+
     }
 }
