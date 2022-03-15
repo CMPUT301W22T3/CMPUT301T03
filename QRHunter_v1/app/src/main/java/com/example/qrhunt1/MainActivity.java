ff8eb6c7c24d984a93d097a71427312f1cf4b983
James Zhang <yuanh30@gmail.com>
2022-03-15 01:06:27 -0600

    pull testing

--- QRHunter_v1/app/src/main/java/com/example/qrhunt1/MainActivity.java
+++ QRHunter_v1/app/src/main/java/com/example/qrhunt1/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.qrhunt1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.qrhunt1.ui.Login.CallbackFragment;
 import com.example.qrhunt1.ui.Login.LoginFragment;
@@ -18,64 +22,34 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.qrhunt1.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity implements CallbackFragment {
-
-    Fragment fragment;
-    FragmentManager fragmentManager;
-    FragmentTransaction fragmentTransaction;
-
-    private ActivityMainBinding binding;
+public class MainActivity extends AppCompatActivity {// implements CallbackFragment {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_login);
 
-        addLoginFragment();
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_gallery, R.id.navigation_map, R.id.navigation_players)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        loginTest();
     }
 
-    public void addLoginFragment(){
-        Fragment fragment = new LoginFragment();
-        ((LoginFragment) fragment).setCallbackFragment(this);
-        fragmentManager = getSupportFragmentManager();
-        fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragmentContainer,fragment);
-        fragmentTransaction.commit();
+    public void loginTest() {
+        Button loginButton = findViewById(R.id.login_btn);
+        TextView create = findViewById(R.id.createnew);
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NaviTest.class);
+                startActivity(intent);
+            }
+        });
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Sign_up.class);
+                startActivity(intent);
+            }
+        });
     }
-
-    public void replaceSignupFragment(){
-        Fragment fragment = new SignupFragment();
-        ((SignupFragment) fragment).setCallbackFragment(this);
-        fragmentManager = getSupportFragmentManager();
-        fragmentTransaction = fragmentManager.beginTransaction();
-        //fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.replace(R.id.fragmentContainer,fragment);
-        fragmentTransaction.commit();
-
-    }
-
-    @Override
-    public void toSignupFragment() {
-        replaceSignupFragment();
-    }
-    @Override
-    public void toLoginFragment(){
-        addLoginFragment();
-    }
-
-
-
 }
--- QRHunter_v1/app/src/main/java/com/example/qrhunt1/NaviTest.java
+++ QRHunter_v1/app/src/main/java/com/example/qrhunt1/NaviTest.java
@@ -1,14 +1,35 @@
 package com.example.qrhunt1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.example.qrhunt1.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class NaviTest extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navi_test);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_gallery, R.id.navigation_map, R.id.navigation_players)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 }
