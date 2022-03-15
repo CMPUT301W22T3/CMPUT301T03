e1aebd7d6feda9259fda4a0f97990815af0c2cd5
James Zhang <yuanh30@gmail.com>
2022-03-15 01:17:59 -0600

    test

--- QRHunter_v1/app/src/main/java/com/example/qrhunt1/Sign_up.java
+++ QRHunter_v1/app/src/main/java/com/example/qrhunt1/Sign_up.java
@@ -2,14 +2,11 @@ package com.example.qrhunt1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<< HEAD
 import android.os.Bundle;
-=======
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
->>>>>>> d5eeeb35ebb5c6d07fac5221d4969914c39e62fd
 
 public class Sign_up extends AppCompatActivity {
 
@@ -17,8 +14,6 @@ public class Sign_up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_signup);
-<<<<<<< HEAD
-=======
 
         Button signUpButton = findViewById(R.id.signup_btn);
 
@@ -29,6 +24,5 @@ public class Sign_up extends AppCompatActivity {
                 startActivity(intent);
             }
         });
->>>>>>> d5eeeb35ebb5c6d07fac5221d4969914c39e62fd
     }
 }
