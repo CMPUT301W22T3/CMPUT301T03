617db9a7a3aaa273dac82e90b8a4f2c4b9b1f01a
James Zhang <yuanh30@gmail.com>
2022-03-15 01:44:51 -0600

    t

--- QRHunter_v1/app/src/main/java/com/example/qrhunt1/ui/gallery/QR_detail_Fragment.java
+++ QRHunter_v1/app/src/main/java/com/example/qrhunt1/ui/gallery/QR_detail_Fragment.java
@@ -16,29 +16,29 @@ import com.example.qrhunt1.R;
  * Use the  factory method to create an instance of this dialog fragment.
  */
 public class QR_detail_Fragment extends DialogFragment {
-//    @Override
+    @Override
 
-//    public Dialog onCreateDialog(Bundle savedInstanceState) {
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
 //        // Use the Builder class for convenient dialog construction
-//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
 
-        // Inflate and set the layout for the dialog
-        // Pass null as the parent view because its going in the dialog layout
-//        builder.setView(inflater.inflate(R.layout.fragment_qr_detail, null))
+         //Inflate and set the layout for the dialog
+         //Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.fragment_qr_detail, null))
 //                // Add action buttons
-//                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int id) {
-//                        // sign in the user ...
-//                    }
-//                })
-//                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-//                    public void onClick(DialogInterface dialog, int id) {
-//                        QR_detail_Fragment.this.getDialog().cancel();
-//                    }
-//                });
-//        // Create the AlertDialog object and return it
-//        return builder.create();
-    //}
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // sign in the user ...
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        QR_detail_Fragment.this.getDialog().cancel();
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
 }
