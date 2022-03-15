4ed3eb5f356d6797e64ebf3678fea60692dcbc56
James Zhang <yuanh30@gmail.com>
2022-03-15 00:58:30 -0600

    #

---
+++ QRHunter_v1/app/src/main/java/com/example/qrhunt1/NaviTest.java
@@ -0,0 +1,14 @@
+package com.example.qrhunt1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NaviTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navi_test);
+    }
+}
