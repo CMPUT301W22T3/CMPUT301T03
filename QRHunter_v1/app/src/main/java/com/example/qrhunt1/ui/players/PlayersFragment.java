82660effdaa9dd019e40e178b7685a65d4830d06
feiyang412 <feiyang2@ualberta.ca>
2022-03-15 22:01:06 -0600

    re-upload changes on Players

--- QRHunter_v1/app/src/main/java/com/example/qrhunt1/ui/players/PlayersFragment.java
+++ QRHunter_v1/app/src/main/java/com/example/qrhunt1/ui/players/PlayersFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.qrhunt1.databinding.FragmentPlayersBinding;
+import com.example.qrhunt1.R;
 
 public class PlayersFragment extends Fragment {
 
@@ -18,20 +22,65 @@ public class PlayersFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        PlayersViewModel playersViewModel =
-                new ViewModelProvider(this).get(PlayersViewModel.class);
+        View view = inflater.inflate(R.layout.fragment_players,container,false);
 
-        binding = FragmentPlayersBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        //final EditText editText = binding.editText;
+        //playersViewModel.getText().observe(getViewLifecycleOwner(), editText::setText);
 
-        final TextView textView = binding.textPlayers;
-        playersViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        Button searchButton = view.findViewById(R.id.search_button);
+        EditText searchUser = view.findViewById(R.id.editText);
+        Button bestQRButton = view.findViewById(R.id.bestQR);
+        Button totalQRsButton = view.findViewById(R.id.totalQRs);
+        Button totalScoreButton = view.findViewById(R.id.totalScore);
+        ListView bestQRList = view.findViewById(R.id.bestQRList);
+        ListView totalQRsList = view.findViewById(R.id.totalQRsList);
+        ListView totalScoreList = view.findViewById(R.id.totalScoreList);
+
+        String username = searchUser.getText().toString();
+        //click this button
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //to searched user's profile
+            }
+        });
+
+        //click this button and show the ranking for best QR
+        bestQRButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bestQRList.setVisibility(View.VISIBLE);
+                totalQRsList.setVisibility(View.GONE);
+                totalScoreList.setVisibility(View.GONE);
+            }
+        });
+
+        //click this button and show the ranking for Total QRs
+        totalQRsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bestQRList.setVisibility(View.GONE);
+                totalQRsList.setVisibility(View.VISIBLE);
+                totalScoreList.setVisibility(View.GONE);
+            }
+        });
+
+        //click this button and show the ranking for Total QRs
+        totalScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bestQRList.setVisibility(View.GONE);
+                totalQRsList.setVisibility(View.GONE);
+                totalScoreList.setVisibility(View.VISIBLE);
+            }
+        });
+
+        return view;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
     }
 }
+
