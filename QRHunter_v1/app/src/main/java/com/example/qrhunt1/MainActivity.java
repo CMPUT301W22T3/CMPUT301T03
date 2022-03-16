9c8c93c59065d47ffb481717fa86e56255501258
James Zhang <yuanh30@gmail.com>
2022-03-16 02:42:34 -0600

    Add current location in map

--- QRHunter_v1/app/src/main/java/com/example/qrhunt1/MainActivity.java
+++ QRHunter_v1/app/src/main/java/com/example/qrhunt1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.qrhunt1;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,7 @@ import com.example.qrhunt1.ui.Login.SignupFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
--- QRHunter_v1/app/src/main/java/com/example/qrhunt1/ui/map/MapFragment.java
+++ QRHunter_v1/app/src/main/java/com/example/qrhunt1/ui/map/MapFragment.java
@@ -8,62 +8,89 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.qrhunt1.MainActivity;
 import com.example.qrhunt1.R;
-import com.example.qrhunt1.databinding.FragmentMapBinding;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     GoogleMap map;
+    SupportMapFragment mapFragment;
+    FusedLocationProviderClient client;
     private static final int REQUEST_CODE = 1;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        MapViewModel mapViewModel =
-                new ViewModelProvider(this).get(MapViewModel.class);
 
         View root = inflater.inflate(R.layout.fragment_map, container, false);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+        mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
+
+        client = LocationServices.getFusedLocationProviderClient(getActivity());
+
+//        if (ContextCompat.checkSelfPermission(getContext(),Manifest.permission.ACCESS_FINE_LOCATION)
+//                == PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+//        }
 
 
         return root;
     }
 
-
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
 
+        // Todo - Switching Fragment not stop the map so when go back to map will load immediately
+        // Todo - MapViews OnResume?
+
         map = googleMap;
+        //LatLng Hub = new LatLng(53.525728, -113.520359);
+        //map.addMarker(new MarkerOptions().position(Hub).title("HUB MALL"));
+        //map.animateCamera(CameraUpdateFactory.newLatLngZoom(Hub, 15));
+        enableMyLocation();
 
-        //enableMyLocation();
+    }
 
-        LatLng Hub = new LatLng(53.525728, -113.520359);
-        map.addMarker(new MarkerOptions().position(Hub).title("HUB MALL"));
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(Hub, 15));
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(getContext(),Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            map.setMyLocationEnabled(true);
+            Task<Location> locationTask = client.getLastLocation();
+            locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+                }
+            });
+        }
     }
 }
