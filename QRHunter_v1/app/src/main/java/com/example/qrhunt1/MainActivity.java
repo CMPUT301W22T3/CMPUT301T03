54f40cc0360f21de5e21d79edf9d24dc4d93962a
Wennn <wenxin6@ualberta.ca>
2022-04-03 01:43:44 -0600

    dialog update

--- QRHunter_v1/app/src/main/java/com/example/qrhunt1/MainActivity.java
+++ QRHunter_v1/app/src/main/java/com/example/qrhunt1/MainActivity.java
@@ -18,9 +18,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.qrhunt1.ui.Login.CallbackFragment;
-import com.example.qrhunt1.ui.Login.LoginFragment;
-import com.example.qrhunt1.ui.Login.SignupFragment;
+//import com.example.qrhunt1.ui.Login.CallbackFragment;
+//import com.example.qrhunt1.ui.Login.LoginFragment;
+//import com.example.qrhunt1.ui.Login.SignupFragment;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
